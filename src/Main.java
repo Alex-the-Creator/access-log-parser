1448f8af13d3240a018e5a3ac57ecd3ece0c74e1
Aleksei Kovalev <alexei67kovalev@gmail.com>
2023-06-20 12:06:31 +0300

    File path checker - add capture

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 import java.io.File;
 
 public class Main {
-
-    public static int successfulAttemptsNumber = 0;
     public static void main(String[] args) {
         /*System.out.println("Введите текст и нажмите <Enter>: ");
         String text = new Scanner(System.in).nextLine();
         System.out.println("Длина текста: " + text.length());*/
+        int successfulAttemptsNumber = 0;
         while(true) {
+            System.out.print("Введите путь к файлу: ");
             String path = new Scanner(System.in).nextLine();
             File file = new File(path);
             boolean fileExists = file.exists();
